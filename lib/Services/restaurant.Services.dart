81fb794f013bb66b8c573af101aa5a5d9627b255
ayamohamed99 <aya199taha@gmail.com>
2020-12-26 17:25:22 +0200

    search & filter

--- lib/Services/restaurant.Services.dart
+++ lib/Services/restaurant.Services.dart
@@ -2,37 +2,38 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:ecommerce/Models/restaurantDetail.dart';
 import 'package:ecommerce/common/Repository.dart';
 
-
-class RestaurantService{
-
+class RestaurantService {
   Repository _rep = Repository("restaurant");
   final String rId;
   List<RestaurantDetail> restaurant;
 
-    final db = Firestore.instance.collection("restaurant");
-
+  final db = Firestore.instance.collection("restaurant");
 
   RestaurantService({this.rId});
 
   Future addRestaurant(RestaurantDetail data) async {
-     return await _rep.addDocument(data.toJson());
+    return await _rep.addDocument(data.toJson());
   }
-  
-  Future <List<RestaurantDetail>> fetchData() async {
+
+  Future<List<RestaurantDetail>> fetchData() async {
     var result = await _rep.getDataCollection();
-    restaurant = result.documents.map((doc)=> RestaurantDetail.fromMap(doc.data, doc.documentID)).toList();
+    restaurant = result.documents
+        .map((doc) => RestaurantDetail.fromMap(doc.data, doc.documentID))
+        .toList();
     return restaurant;
   }
-  Future<void> removeDocument(String id) async{
-     return await _rep.removeDocument(id);
+
+  Future<void> removeDocument(String id) async {
+    return await _rep.removeDocument(id);
   }
-  Future update(RestaurantDetail data , String rId) async{
+
+  Future update(RestaurantDetail data, String rId) async {
     return await _rep.updateDocument(data.toJson(), rId);
   }
-  Future<RestaurantDetail> getById(String rId)async {
-    var result =  await db.where("rId",isEqualTo: rId).getDocuments();
+
+  Future<RestaurantDetail> getById(String rId) async {
+    var result = await db.where("rId", isEqualTo: rId).getDocuments();
     var restaurant = result.documents.first;
     return RestaurantDetail.fromMap(restaurant.data, restaurant.documentID);
   }
-  
 }
